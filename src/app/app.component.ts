7de3205da7cdcdecc0c73945ecceeb0c73240681
Heleno Jimenez de la Cruz <heleno.jimenez@mx1.ibm.com>
2017-09-01 10:40:50 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { OAuth } from 'forcejs/oauth';
+import { DataService } from 'forcejs/data-service';
 
 @Component({
   templateUrl: 'app.html'
@@ -17,6 +19,16 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      let oauth = OAuth.createInstance();
+      oauth.login().then(oauthResult => {
+        DataService.createInstance(oauthResult);
+        console.log("Logged Into Salesforce Successfully");
+      });
+
+      //  To get data from Salesforce add
+      let sfdc = DataService.getInstance();
+      sfdc.query().then(data => { console.log("DATA" + data) }).catch(error => console.log(error));
     });
   }
 }
